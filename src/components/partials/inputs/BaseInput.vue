dc574e33d1fb8ad35bfd54f189be8e8e8bcc0fa8
nagwan <nagwan@boostiny.com>
2023-05-05 15:47:32 +0300

    create baseInput component

--- src/components/partials/inputs/BaseInput.vue
+++ src/components/partials/inputs/BaseInput.vue
@@ -1,7 +1,42 @@
 <script>
 export default {
   components: {},
-  props: {},
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'text',
+      validator(value) {
+        return ['text', 'number'].includes(value);
+      },
+    },
+    value: {
+      type: [Number, String],
+      required: true,
+    },
+
+    isRequired: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    label: {
+      type: String,
+      required: false,
+      default: null,
+    },
+
+    placeholder: {
+      type: String,
+      required: true,
+    },
+
+    inputId: {
+      type: String,
+      required: true,
+    },
+  },
   data() {
     return {};
   },
@@ -10,8 +45,40 @@ export default {
 };
 </script>
 <template>
-  <div class>
-    Base Input
-  </div>
+    <div class>
+        <label class="d-inline-block m-b-4" v-if="label" :htmlFor="inputId">
+            <span class="text-secondary-50">{{ label }}</span>
+            <span v-if="isRequired" class="text-danger-100">*</span>
+        </label>
+
+        <input
+            :id="inputId"
+            class="w-100 radius-8 p-10 text-secondary-50 border-secondary-50"
+            :type="type"
+            :placeholder="placeholder"
+            :value="value"
+            @change="$emit('update:value', $event.target.value)"
+        />
+    </div>
 </template>
-<style scoped lang='sass'></style>
+<style scoped lang='sass'>
+input
+    height: 40px
+    outline: none !important
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+input:-webkit-autofill:active
+    outline: none !important
+    box-shadow: none !important
+    -webkit-box-shadow: 0 0 0 30px white inset !important
+::placeholder
+    font-family: primary-medium !important
+    color: #F3D6DF !important
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button
+    -webkit-appearance: none
+    margin: 0
+input[type=number]
+    -moz-appearance: textfield
+</style>
